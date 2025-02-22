fb7d635293f7057390162f223b0244d9b3f87aab
eswareddy26 <eswareddy26@gmail.com>
2025-02-22 17:01:47 +0530

    hi

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Post
 # Home Page
-@login_required
+
 def home(request):
     posts = Post.objects.all().order_by("-created_at")
     return render(request, "home.html", {"posts": posts})
